314b0af8f632b59653b259b8709726f1463d3e2a
Andrei Latchescu <andrei@nextbyte.ro>
2013-08-02 19:26:26 +0300

    fix customer name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,6 @@ class Customer < ActiveRecord::Base
   attr_accessible :address_1, :address_2, :city, :company, :first_name, :last_name, :postcode, :contact_number
 
   def name
-    first_name
+    return "#{first_name} #{last_name}"
   end
 end
