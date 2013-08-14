2c19931f6ad94de8a0583f2050fd81ec9499dd6c
unknown <marinlx@LYN-GBYQXW1.global.ad.toro.com>
2013-08-14 20:24:04 +0300

    Add show invoices for all customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,4 +1,23 @@
 ActiveAdmin.register Customer do
+  index do
+    column :id
+    column :first_name
+    column :last_name
+    column :company
+    column :address_1
+    column :address_2
+    column :city
+    column :postcode
+    column :created_at
+    column :updated_at
+    column :contact_number
+    column :email
+    column "Issued invoices", :id do |invoice|
+            link_to "Show invoices","/admin/invoices?utf8=%E2%9C%93&q%5Bcustomer_id_eq%5D="+ invoice.id.to_s+ '&commit=Filter&order=id_desc'
+    end
+    default_actions
+
+  end
   member_action :show_invoice do
     @customer = Customer.find(params[:id])
 
