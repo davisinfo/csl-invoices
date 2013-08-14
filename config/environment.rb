1405601164c475296ce3f5b843c6e66de1bfee7f
unknown <marinlx@LYN-GBYQXW1.global.ad.toro.com>
2013-08-14 21:02:31 +0300

    Added e-mail config

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Invoices::Application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['app17274229@heroku.com'],
+    :password       => ENV['4rthntgq'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}
