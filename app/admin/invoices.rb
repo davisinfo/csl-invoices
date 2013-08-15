74f21e4b3f36c39890aca2080361178966b97c56
unknown <marinlx@LYN-GBYQXW1.global.ad.toro.com>
2013-08-15 17:03:14 +0300

    Added fix for edit incrementation and due date field2

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -80,7 +80,7 @@ ActiveAdmin.register Invoice do
       row :invoice_number
       row :customer
       row :due_date do |obj|
-        d = obj.due_date
+         d = obj.due_date.to_i
         obj.created_at + d.day
       end
       row :created_at
