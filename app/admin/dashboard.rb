946a16bec46f657abc3a8b0d9a55992fb30acc56
lilian21 <lilian.marin21@gmail.com>
2013-08-16 22:45:05 +0300

    Finall3

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,36 +1,19 @@
 ActiveAdmin.register_page "Dashboard" do
-
-  menu :priority => 1, :label => proc { I18n.t("active_admin.dashboard") }
-
-      content :title => proc { I18n.t("active_admin.dashboard") } do
-
-         section "Recent Invoices" do
-           table_for Invoice.order("created_at desc").limit(5) do
-             column :id do |invoice|
-               link_to invoice.id, [:admin, invoice]
-             end
-
-             column :customer
-             column :created_at
-
-           end
-
-           strong {link_to "View All Invoices", admin_invoices_path}
-         end
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-
-    # end
-  end # content
-end
+  menu :priority => 1
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Recent Invoices" do
+          table_for Invoice.order("created_at desc").limit(5) do
+            column :invoice_number do |invoice|
+              link_to invoice.invoice_number, [:admin, invoice]
+            end
+            column :customer
+            column :created_at
+          end
+          strong { link_to "View All Invoices", admin_invoices_path }
+        end
+      end
+    end
+  end
+end  
