2071fd827c9e5c5d0ea51803fc3e1af24d916176
lilian21 <lilian.marin21@gmail.com>
2013-08-16 21:41:41 +0300

    Changed dashboard appearance

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,18 +1,24 @@
 ActiveAdmin.register_page "Dashboard" do
+  menu :priority => 1
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     columns do
       column do
-        panel "Recent Products" do
+        panel "Recent Invoices" do
           table_for Invoice.order("created_at desc").limit(5) do
             column :invoice_number do |invoice|
               link_to invoice.invoice_number, [:admin, invoice]
             end
-            column :customer
+
             column :created_at
+            column :due_date do |obj|
+              obj.due_date.to_i
+              (obj.created_at + obj.due_date.to_i.day).strftime("%B %d, %Y %H:%M")
+            end
           end
           strong { link_to "View All Invoices", admin_invoices_path }
         end
       end
+    end
 
 # Here is an example of a simple dashboard with columns and panels.
     #
@@ -30,6 +36,4 @@ ActiveAdmin.register_page "Dashboard" do
 
     # end
   end # content
-  end
 end
-
